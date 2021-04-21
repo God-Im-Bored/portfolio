2ad527a0c185be5fefba4b162ab22cde87138ab8
Aram <aram_martin@alumni.brown.edu>
2021-04-21 11:15:06 -0500

    FIX: spelling error in bio

--- text.js
+++ text.js
@@ -1,5 +1,5 @@
 const bioMap = {
-    aboutMe: "I'm Aram Martin, a full-stack software engineer and web developer based in Chicago, IL. I've spent the last year researching, desgning, and implementing the best solutions to web applications, including: writing effective web APIs, developing and managing well-functioning databases, creating security and data protection features, and debugging code for optimal performance. Prior to, I spent the previous 4 years working in healthcare and clinical research where I compiled, organized and analyzed to assist in delivering optimal healthcare management.",
+    aboutMe: "I'm Aram Martin, a full-stack software engineer and web developer based in Chicago, IL. I've spent the last year researching, designing, and implementing the best solutions to web applications, including: writing effective web APIs, developing and managing well-functioning databases, creating security and data protection features, and debugging code for optimal performance. Prior to, I spent the previous 4 years working in healthcare and clinical research where I compiled, organized and analyzed to assist in delivering optimal healthcare management.",
     interest: "When I'm not coding, you can find me reading my favorite Conant tales, working out and playing basketball, napping, gardening, or watching my favorite show, Lupin the 3rd."
 }
 
